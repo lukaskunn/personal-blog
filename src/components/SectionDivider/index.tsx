c75be8fabb1f4ee54589bb237d05eb37bd3f3d5d
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-12-28 21:16:39 -0300

    delete all files

--- src/components/SectionDivider/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import style from "./SectionDivider.module.scss";
-
-const SectionDivider = () => {
-  return <div className={style.divider} />;
-};
-
-export default SectionDivider;
