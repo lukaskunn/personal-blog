d1f87ac937937864482589e120f686665cabcb3e
Lucas Oliveira <lucassioliveira098@gmail.com>
2026-06-05 20:38:51 -0300

    feat: add loading progress on post page

--- src/app/post/[slug]/layout.tsx
+++ src/app/post/[slug]/layout.tsx
@@ -1,7 +1,14 @@
+import ReadingProgress from "@/components/ReadingProgress";
+
 export default function PostLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <>
+      <ReadingProgress />
+      {children}
+    </>
+  );
 }
